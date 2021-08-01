1b026193ee880f3e0f00efcd806268166a83220c
LucioMart <luchoweb15@gmail.com>
2021-08-01 20:36:11 -0300

    definiendo la logica del punto de entrada (app.js), controller y router

--- app.js
+++ app.js
@@ -2,16 +2,19 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
+const rutasMain = require('./routers/main')
+
+app.use('/', rutasMain)
+
 app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'home.html'))
-})
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'views', 'home.html'))
+// })
 
-app.get('/about', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'about.html'))
-})
-
-app.listen(3030, () => {
-    console.log('Servidor funcionando.')
-})
+// app.get('/about', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'views', 'about.html'))
+// })
+ app.listen(3030, () => {
+     console.log('Servidor funcionando.')
+ })
---
+++ controllers/mainController.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router()
+const path = require('path')
+
+
+const controlador = {
+    home : (req, res) => {
+        res.sendFile(path.join(__dirname, '../views', 'home.html'))
+    },
+    about : (req, res) => {
+        res.sendFile(path.join(__dirname, '../views', 'about.html'))
+    }
+}
+
+
+module.exports = controlador
