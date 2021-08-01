f9536659710465b674db14f40fedb6983654fee4
LucioMart <luchoweb15@gmail.com>
2021-08-01 11:10:56 -0300

    Colocando los archivos para realizar el tp. Levantando el servidor, con la forma actual, para visualizar el contenido de las vistas.

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const express = require('express')
-
+const path = require('path')
 const app = express()
 
-app.listen(3000, () => {
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'home.html'))
+})
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'about.html'))
+})
+
+app.listen(3030, () => {
     console.log('Servidor funcionando.')
 })
